eb29df3ff8e1db841d43ef51ec0d96e29911e29b
David Winterbottom <david_winterbottom@hotmail.com>
2014-08-08 17:52:34 +0200

    added java on windows...

--- setup/setup_vim.sh
+++ setup/setup_vim.sh
@@ -14,35 +14,32 @@ else
   echo "unknown environment `uname`"
 fi
 
-# git pull and install dotfiles as well
-cd $HOME
-
-if [ -d ".dotfiles"]; then
-  cd .dotfiles
-  git pull
-  cd ..
-else
-  git clone https://github.com/DavidWinterbottom/dotfiles.git .dotfiles
-fi
-
 if [[ `uname` == "MINGW32_NT-6.2" || `uname` == "MINGW32_NT-6.1" ]]; then
 
-  cp -f .dotfiles/.screenrc .
-  cp -f .dotfiles/.bash_profile .
-  cp -f .dotfiles/.bashrc .
-  cp -f .dotfiles/.bashrc_custom .
-  cp -f .dotfiles/.gitconfig .
-  cp -f .dotfiles/.npmrc .
-  cp -f .dotfiles/.inputrc .
-  cp -f .dotfiles/.ctags .
+  # git pull and install dotfiles as well
+  cd $HOME
+  git clone https://github.com/DavidWinterbottom/dotfiles.git .dotfiles
+  cp .dotfiles/.screenrc .
+  cp .dotfiles/.bash_profile .
+  cp .dotfiles/.bashrc .
+  cp .dotfiles/.bashrc_custom .
+  cp .dotfiles/.gitconfig .
+  cp .dotfiles/.npmrc .
+  cp .dotfiles/.inputrc .
+  cp .dotfiles/.ctags .
 
   # copy over the bin directory
-  cp -rf .dotfiles/bin .
+  cp -r .dotfiles/bin .
 
   # copy over the conEum settings file
   cp .dotfiles/ConEmu.xml /c/local/tools/ConEmu
 
+  # git pull and install vim setup
+  git clone https://github.com/DavidWinterbottom/dotvim.git vimfiles
+  cd ~/vimfiles
+
 elif [[ `uname` == "Linux" || `uname` == "Darwin" ]]; then
+  echo "setting environment for " `uname`
 
   # git pull and install dotfiles as well
   cd $HOME
@@ -59,4 +56,12 @@ elif [[ `uname` == "Linux" || `uname` == "Darwin" ]]; then
   # link the bin directory
   ln -s .dotfiles/bin .
 
+  # git pull and install vim setup
+  git clone https://github.com/DavidWinterbottom/dotvim.git .vim
+
+  cd ~/.vim
 fi
+
+git submodule init
+git submodule update
+cd -
